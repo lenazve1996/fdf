c8b5294b6435e8dbb4702ce11a2adc4e3af5e03e
Allen Yajirobe <ayajirob@oa-c1.msk.21-school.ru>
2021-12-18 20:08:47 +0300

    a

--- minilibx_macos/fdf.c
+++ minilibx_macos/fdf.c
@@ -22,6 +22,13 @@ int main()
 {
 	void    *ptr_to_connection;
 	void    *ptr_to_window;
+	void	*new_image;
+	int		n;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+
 	
 	ptr_to_connection = mlx_init();
 	if (!ptr_to_connection)
@@ -29,11 +36,14 @@ int main()
 		ft_putstr_fd("Failed to set up the connection to the graphical system", 1);
 		return(1);
 	}
-	ptr_to_window = mlx_new_window(ptr_to_connection, 1920, 1080,"fdf");
+	ptr_to_window = mlx_new_window(ptr_to_connection, 1920, 1080, "fdf");
 	if (!ptr_to_window)
 	{
 		ft_putstr_fd("Failed to open a new window", 1);
 		return(1);		
 	}
+	new_image = mlx_new_image(ptr_to_connection, 1280, 720);
+	addr = mlx_get_data_addr(new_image, &bits_per_pixel, &line_length, &endian);
+	n = mlx_put_image_to_window(ptr_to_connection, ptr_to_window, new_image, 0, 0);
 	mlx_loop(ptr_to_connection);
 }
